b8cb7464c32ca10ac071bf3e3ee62114694ad1cc
ivan ferez <ivanferez.web@gmail.com>
2022-05-10 23:28:38 -0300

    Cambio de orden en variables

--- src/ar/unlam/edu/pb2/Calculadora.java
+++ src/ar/unlam/edu/pb2/Calculadora.java
@@ -13,7 +13,7 @@ public class Calculadora {
 		Integer resultado = 0; 
 
 		do{ 
-			System.out.println("Ingrese su operación \n 1 para multiplicar \n 2 para dividir \n 3 para sumar \n 4 para restar"); 
+			System.out.println("Ingrese su operación \n 1 para dividir \n 2 para multiplicar \n 3 para sumar \n 4 para restar"); 
 			opcion = teclado.nextInt(); 
 		} while (opcion < 1 || opcion > 4);
 		System.out.println("Ingrese el primer numero");
@@ -22,12 +22,12 @@ public class Calculadora {
 		b = teclado.nextInt();
 
 		switch (opcion) {
-		case 1:
-			 System.out.println("La opcion elegida es la 1");
-			resultado = a * b;
-			break;
 		case 2:
 			 System.out.println("La opcion elegida es la 2");
+			resultado = a * b;
+			break;
+		case 1:
+			 System.out.println("La opcion elegida es la 1");
 			resultado = a / b;
 			break;
 		case 3:
